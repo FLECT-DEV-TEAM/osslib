189f326b05e1022fc72784bafaf9f72b70a13b74
k-shunji <k-shunji@flect.co.jp>
2013-06-03 18:20:37 +0900

    Add some new projects

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,24 +1,35 @@
 package controllers;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import play.mvc.Controller;
 import play.i18n.Lang;
 
 public class Application extends Controller {
 	
-	private static final List<String> PROJECT_LIST = new ArrayList<String>();
+	private static final Map<String, ProjectInfo> PROJECTS = new HashMap<String, ProjectInfo>();
+	
+	private static void add(String name, boolean hasEnglish, boolean hasGithub) {
+		PROJECTS.put(name, new ProjectInfo(name, hasEnglish, hasGithub));
+	}
 	
 	static {
-		PROJECT_LIST.add("sendgrid4j");
-		PROJECT_LIST.add("papertrailTool");
-		PROJECT_LIST.add("excel2canvas");
-		PROJECT_LIST.add("flectCommon");
-		PROJECT_LIST.add("flectSoap");
-		PROJECT_LIST.add("flectSalesforce");
-		PROJECT_LIST.add("papertrail-log-analyze");
+		add("excel2canvas", true, true);
+		add("sendgrid4j", true, true);
+		add("papertrailTool", true, true);
+		add("flectCommon", true, true);
+		add("flectSoap", true, true);
+		add("flectSalesforce", true, true);
+		
+		add("papertrail-log-analyze", true, true);
+		add("sqlsync", false, true);
+		
+		add("heroku-buildpack-ffmpeg", true, true);
+		
+		add("excelReport", true, false);
+		add("forceexplorer", true, false);
 	}
 	
 	public static void index() {
@@ -37,14 +48,28 @@ public class Application extends Controller {
 	}
 	
 	public static void view(String filename) {
-		if (!PROJECT_LIST.contains(filename)) {
+		ProjectInfo project = PROJECTS.get(filename);
+		if (project == null) {
 			notFound();
 		}
 		File f = new File("app/views/contents/" + filename + ".html");
 		if (!f.exists()) {
 			f = new File("app/views/contents/underconstruction.html");
 		}
-		renderTemplate("contents/" + f.getName(), filename);
+		renderTemplate("contents/" + f.getName(), filename, project);
+	}
+	
+	public static class ProjectInfo {
+		
+		public String name;
+		public boolean hasEnglish;
+		public boolean hasGithub;
+		
+		public ProjectInfo(String name, boolean hasEnglish, boolean hasGithub) {
+			this.name = name;
+			this.hasEnglish = hasEnglish;
+			this.hasGithub = hasGithub;
+		}
 	}
 
 }

7b6ea242da4fa3611e7ac6e637ae33d4dcefb187
k-shunji <k-shunji@flect.co.jp>
2013-12-06 20:33:43 +0900

    Modify protocol of jquery.socialbutton.js

--- public/javascripts/ext/socialbutton/jquery.socialbutton.js
+++ public/javascripts/ext/socialbutton/jquery.socialbutton.js
@@ -272,7 +272,7 @@ $.fn.socialbutton = function(service, options) {
 			title: document.title
 		},
 		hatena_oldstyle: {
-			button: 'http://d.hatena.ne.jp/images/b_append.gif',
+			button: '//d.hatena.ne.jp/images/b_append.gif',
 			url: document.URL,
 			height: 13,
 			padding: 7
@@ -366,12 +366,12 @@ function socialbutton_mixi_check(target, options, defaults, index, max_index)
 		'data-button': button
 	});
 
-	var tag = '<a href="http://mixi.jp/share.pl" class="mixi-check-button"' + attr + '>Check</a>';
+	var tag = '<a href="//mixi.jp/share.pl" class="mixi-check-button"' + attr + '>Check</a>';
 
 	$(target).html(tag);
 
 	if (index == max_index) {
-		$('body').append('<script type="text/javascript" src="http://static.mixi.jp/js/share.js"></script>');
+		$('body').append('<script type="text/javascript" src="//static.mixi.jp/js/share.js"></script>');
 	}
 }
 
@@ -415,7 +415,7 @@ function socialbutton_mixi_like(target, options, defaults, index, max_index)
 	});
 
 	var attr = merge_attributes({
-		src: 'http://plugins.mixi.jp/favorite.pl?' + params,
+		src: '//plugins.mixi.jp/favorite.pl?' + params,
 		scrolling: 'no',
 		frameborder: '0',
 		allowTransparency: 'true',
@@ -499,7 +499,7 @@ function socialbutton_facebook_like(target, options, defaults, index, max_index)
 		"appId": appId
 	});
 
-	var tag = '<iframe src="http://www.facebook.com/plugins/like.php?' + params + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' + width + 'px; height:' + height + 'px;" allowTransparency="true"></iframe>';
+	var tag = '<iframe src="//www.facebook.com/plugins/like.php?' + params + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' + width + 'px; height:' + height + 'px;" allowTransparency="true"></iframe>';
 
 	$(target).html(tag);
 }
@@ -518,7 +518,7 @@ function socialbutton_facebook_share(target, options, defaults, index, max_index
 	var tag = '<a name="fb_share"' + attr + '>' + text + '</a>';
 
 	if(index == 0) {
-		tag += '<script type="text/javascript" src="http://static.ak.fbcdn.net/connect.php/js/FB.Share"></script>';
+		tag += '<script type="text/javascript" src="//static.ak.fbcdn.net/connect.php/js/FB.Share"></script>';
 	}
 
 	$(target).html(tag);
@@ -543,12 +543,12 @@ function socialbutton_twitter(target, options, defaults, index, max_index)
 		'data-related': related
 	});
 
-	var tag = '<a href="http://twitter.com/share" class="twitter-share-button"' + attr + '>Tweet</a>';
+	var tag = '<a href="//twitter.com/share" class="twitter-share-button"' + attr + '>Tweet</a>';
 
 	$(target).html(tag);
 
 	if (index == max_index) {
-		$('body').append('<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>');
+		$('body').append('<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>');
 	}
 }
 
@@ -598,7 +598,7 @@ function socialbutton_gree_sf(target, options, defaults, index, max_index)
 		'height': height
 	});
 
-	var tag = '<iframe src="http://share.gree.jp/share?' + params + '" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" style="border:none; overflow:hidden; width:' + width + 'px; height:' + height + 'px;" allowTransparency="true"></iframe>';
+	var tag = '<iframe src="//share.gree.jp/share?' + params + '" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" style="border:none; overflow:hidden; width:' + width + 'px; height:' + height + 'px;" allowTransparency="true"></iframe>';
 
 	$(target).html(tag);
 }
@@ -629,7 +629,7 @@ function socialbutton_evernote(target, options, defaults, index, max_index)
 		button == 'site-mem-22' ||
 		button == 'site-mem-16'
 		) {
-			button = 'http://static.evernote.com/' + button + '.png';
+			button = '//static.evernote.com/' + button + '.png';
 	}
 
 	var clipoptions = {
@@ -644,7 +644,7 @@ function socialbutton_evernote(target, options, defaults, index, max_index)
 	};
 
 	if (index == 0) {
-		$('body').append('<script type="text/javascript" src="http://static.evernote.com/noteit.js"></script>');
+		$('body').append('<script type="text/javascript" src="//static.evernote.com/noteit.js"></script>');
 	}
 
 	var tag = $(document.createElement('a'))
@@ -670,15 +670,15 @@ function socialbutton_hatena(target, options, defaults, index, max_index)
 	title = htmlspecialchars(title);
 
 	var attr = merge_attributes({
-		'href': 'http://b.hatena.ne.jp/entry/' + url,
+		'href': '//b.hatena.ne.jp/entry/' + url,
 		'class': 'hatena-bookmark-button',
 		'data-hatena-bookmark-title': title,
 		'data-hatena-bookmark-layout': layout,
 		'title': 'このエントリーをはてなブックマークに追加'
 	});
 	
-	var tag = '<a' + attr + '><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>'
-			+ '<script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>';
+	var tag = '<a' + attr + '><img src="//b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>'
+			+ '<script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>';
 
 	$(target).html(tag);
 }
@@ -694,8 +694,8 @@ function socialbutton_hatena_oldstyle(target, options, defaults, index, max_inde
 	url = htmlspecialchars(url);
 
 	var tag = '<span style="font-size: ' + height + 'px; line-height: 100%; ">'
-			+ '<span style="padding-right: ' + padding + 'px"><a href="http://b.hatena.ne.jp/entry/add/' + url + '" target="_blank"><img src="' + button + '" style="border: none; vertical-align: text-bottom" /></a></span>'
-			+ '<a href="http://b.hatena.ne.jp/entry/' + url + '" target="_blank"><img src="http://b.hatena.ne.jp/entry/image/' + url + '" style="border: none; vertical-align: text-bottom" /></a>'
+			+ '<span style="padding-right: ' + padding + 'px"><a href="//b.hatena.ne.jp/entry/add/' + url + '" target="_blank"><img src="' + button + '" style="border: none; vertical-align: text-bottom" /></a></span>'
+			+ '<a href="//b.hatena.ne.jp/entry/' + url + '" target="_blank"><img src="//b.hatena.ne.jp/entry/image/' + url + '" style="border: none; vertical-align: text-bottom" /></a>'
 			+ '</span>';
 
 	$(target).html(tag);
@@ -752,7 +752,7 @@ function socialbutton_google_plusone(target, options, defaults, index, max_index
 		}
 
 		if (typeof gapi === 'undefined' || typeof gapi.plusone === 'undefined') {
-			$('body').append('<script type="text/javascript" src="https://apis.google.com/js/plusone.js">' + script_params + '</script>');
+			$('body').append('<script type="text/javascript" src="//apis.google.com/js/plusone.js">' + script_params + '</script>');
 		} else {
 			gapi.plusone.go();
 		}
@@ -776,7 +776,7 @@ function socialbutton_pinterest(target, options, defaults, index, max_index)
 		'description': description
 	});
 
-	var tag = '<a href="http://pinterest.com/pin/create/button/?' + params + '" class="pin-it-button" count-layout="' + button +'"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>'
+	var tag = '<a href="//pinterest.com/pin/create/button/?' + params + '" class="pin-it-button" count-layout="' + button +'"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>'
 
 	$(target).html(tag);
 
